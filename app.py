38b1436b5ed734fee519dadd0d697570cf1f87e7
P4ttyCakes <patricklu1000@gmail.com>
2025-04-13 20:11:16 -0400

    Final?

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from simulation import simulate_option, black_scholes
 import pandas as pd
+from st_social_media_links import SocialMediaIcons
+
+
 
 
 # Sidebar Inputs
@@ -11,7 +14,7 @@ S = st.sidebar.slider('Stock Price (S)', 0.0, 1000.0, 500.0, 1.0)
 K = st.sidebar.slider('Strike Price (K)', 0.0, 1000.0, 500.0, 1.0)
 sigma = st.sidebar.slider('Volatility (σ)', 0.1, 0.5, 0.3, 0.05)
 r = st.sidebar.slider('Risk-free Rate (r)', 0.01, 0.1, 0.05, 0.01)
-t = st.sidebar.slider('Time to Expiration (years)', 0.25, 10.0, 5.0, 0.25)
+t = st.sidebar.slider('Time to Expiration (years)', 0.25, 2.0, 1.0, 0.25)
 
 # Black-Scholes Fair Value
 bs_premium = black_scholes(S, K, sigma, r, t, option_type)
@@ -35,11 +38,11 @@ actual_premium = st.number_input("What did you pay for the option?", min_value=0
 if st.button("Run Geometric Brownian Motion Simulation"):
     sim_data = simulate_option(S, K, sigma, r, t, option_type=option_type)
 
-    st.subheader('Simulated Option Outcome')
+    st.subheader('Simulated Option Value & Stock Price Path')
     col1, col2, col3 = st.columns(3)
     col1.metric("Price Paid", f"${actual_premium:.2f}")
     col2.metric("Final Stock Price", f"${sim_data['final_price']:.2f}")
-    col3.metric("Profit / Loss", f"${sim_data['payoff'] - actual_premium:.2f}")
+    col3.metric("PNL", f"${sim_data['payoff'] - actual_premium:.2f}")
 
     # Create DataFrame for charting
     price_path = pd.Series(sim_data['path'], name='Stock Price')
@@ -57,5 +60,17 @@ if st.button("Run Geometric Brownian Motion Simulation"):
 
     st.line_chart(chart_df[['Stock Price', 'Strike Price']], use_container_width=True)
 
-    st.subheader('Payoff Over Time')
+    st.subheader('PNL over Time')
     st.line_chart(chart_df[['Payoff']], use_container_width=True)
+
+social_media_links = [
+    "www.linkedin.com/in/patrick-lu-0376152a8",
+    "https://github.com/P4ttyCakes"]
+
+social_media_icons = SocialMediaIcons(social_media_links)
+# Divider and Social Media Footer
+st.markdown("---")
+st.markdown("### Connect with Me (Patrick Lu)")
+social_media_icons.render()
+
+
